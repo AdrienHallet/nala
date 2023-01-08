114deb3232e0444da7e0b3cf8fd9cad6317405f3
Adrien Hallet <adrienhallet01@gmail.com>
2023-01-08 22:28:29 +0100

    feat: Avatar layout and image

---
+++ src/ui/specifics/layout/avatar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+
+	export let username: string;
+	export let avatarUrl: string;
+</script>
+
+<div class="relative flex cursor-pointer rounded-full bg-zinc-700 p-1 hover:bg-zinc-300">
+	<img src={avatarUrl} alt={username} class="h-6 max-w-none" />
+</div>
