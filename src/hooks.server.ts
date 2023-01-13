d0330f47b7b6085136c38c9d0f4aee79d5d12c1f
Adrien Hallet <adrienhallet01@gmail.com>
2023-01-14 00:42:52 +0100

    feat: Access token persistence in session

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,7 @@ import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
 import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
 import type { Provider } from "@auth/core/providers"
+import type { ISessionToken } from "$core/model/session-token"
 
 /**
  * 
@@ -16,4 +17,19 @@ export const handle = SvelteKitAuth({
             }
         }) as Provider
     ],
-})
+    callbacks: {
+        async session({ session, token }) {
+            (session as ISessionToken).accessToken = token.accessToken as string;
+            return session
+        },
+
+        async jwt({ token, account }) {
+            // Persist the OAuth access_token and or the user id to the token right after signin
+            if (account) {
+                token.accessToken = account.access_token
+            }
+            return token
+        }
+    },
+}
+)
