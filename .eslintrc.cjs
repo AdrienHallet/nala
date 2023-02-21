cbb8c3f1f751c5a6506934712b78675c5ce33eab
Adrien Hallet <adrienhallet01@gmail.com>
2023-02-21 21:38:00 +0100

    chore: Remove no-undef rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,9 @@ module.exports = {
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
+	rules: {
+		'no-undef': 'off',
+	},
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020
