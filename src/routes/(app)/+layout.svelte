1eb21e121fe5e9c56097fd5d77a97a07607e0e9e
Adrien Hallet <adrienhallet01@gmail.com>
2023-03-08 23:28:21 +0100

    chore: layout improvements

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -17,7 +17,7 @@
 	$: collapse = $configuration.ui.menuCollapsed;
 </script>
 
-<div class="z-50 flex h-16 w-full items-center bg-zinc-900">
+<div class="z-50 flex h-16 min-h-[4rem] w-full items-center bg-zinc-900">
 	<div
 		class="relative flex h-full items-center transition-width duration-500 {collapse
 			? 'sm:w-16'
@@ -57,23 +57,74 @@
 <div class="z-0 flex w-full flex-grow flex-row overflow-auto overflow-x-hidden">
 	<div
 		id="Main"
-		class="absolute z-50 h-full max-h-full w-full transform items-start justify-start overflow-x-hidden overflow-y-hidden bg-zinc-900 transition-all duration-500 ease-in-out sm:relative sm:h-auto xl:translate-x-0 xl:rounded-r {collapse
+		class="absolute z-50 h-[calc(100vh-4rem)] w-full transform items-start justify-start overflow-x-hidden overflow-y-hidden bg-zinc-900 transition-all duration-500 ease-in-out sm:relative sm:h-auto xl:translate-x-0 xl:rounded-r {collapse
 			? '-translate-x-full sm:flex sm:w-16 sm:translate-x-0'
 			: 'sm:w-64'} "
 	>
 		<div
-			class="flex h-full w-full grow flex-col items-center justify-start space-y-3 overflow-y-auto overflow-x-hidden p-5"
+			class="flex h-full w-full grow flex-col items-center justify-start overflow-y-auto overflow-x-hidden py-2"
 		>
 			<SidebarItem bind:collapse link="/">
-				<Dashboard classes="w-6 absolute" slot="icon" />
+				<Dashboard classes="w-6" slot="icon" />
 				Dashboard
 			</SidebarItem>
 			<SidebarItem bind:collapse link="/transactions">
-				<TableCells classes="w-6 absolute" slot="icon" />
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
+				Transactions
+			</SidebarItem><SidebarItem bind:collapse link="/transactions">
+				<TableCells classes="w-6" slot="icon" />
 				Transactions
 			</SidebarItem>
 			<SidebarItem bind:collapse link="/categories">
-				<Tag classes="w-6 absolute" slot="icon" />
+				<Tag classes="w-6" slot="icon" />
 				Categories
 			</SidebarItem>
 			<div class="flex w-full flex-col items-center">
