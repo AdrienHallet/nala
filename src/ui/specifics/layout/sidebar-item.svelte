6562da031e273110b2c535b8bce2713f969b0b68
Adrien Hallet <adrienhallet01@gmail.com>
2023-03-12 20:54:32 +0100

    feat: Auto close menu on mobile after click

--- src/ui/specifics/layout/sidebar-item.svelte
+++ src/ui/specifics/layout/sidebar-item.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
+	import { toggleMenu } from '../../../core/configuration/operations';
+
 	export let collapse = false;
 	export let link = '#';
+
+	let item: Element;
+
+	function onMenuItemClick() {
+		if (item.clientWidth === window.innerWidth) {
+			toggleMenu();
+		}
+	}
 </script>
 
-<a class="grid min-h-[3rem] w-full grid-cols-[4rem_1fr] items-center hover:bg-zinc-700" href={link}>
+<a
+	bind:this={item}
+	on:click={onMenuItemClick}
+	class="grid min-h-[3rem] w-full grid-cols-[4rem_1fr] items-center hover:bg-zinc-700"
+	href={link}
+>
 	<div class="justify-self-center">
 		<slot name="icon" />
 	</div>
