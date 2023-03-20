25987c2efb907be8dc7294482cdbaa1eaf6f7c71
Adrien Hallet <adrienhallet01@gmail.com>
2023-03-20 22:23:00 +0100

    Feat: Monthly display

--- src/routes/(app)/reports/+page.svelte
+++ src/routes/(app)/reports/+page.svelte
@@ -1,8 +1,32 @@
 <script lang="ts">
 	import { monthlyState } from '$core/database/balance/state.js';
+	import { loading } from '$core/loading/state.js';
+	import VirtualScroll from '$ui/components/virtual-scroll.svelte';
+	import Loading from '$ui/components/loading.svelte';
+	import { getCategories } from '../../../core/database/category/operations';
+	import { getTransactions } from '../../../core/database/transaction/operations';
+	import { MONTHLY_LAYOUT } from './constants.js';
+	import MonthlyItem from './MonthlyItem.svelte';
+
+	getTransactions();
+	getCategories();
 </script>
 
-{#each $monthlyState.reverse() as monthly}
-	<br />
-	{monthly.month} | {monthly.income} | {monthly.expense}
-{/each}
+{#if $loading.database || $loading.transactions || $loading.categories}
+	<Loading />
+{:else}
+	<div class="h-[calc(100vh-66px)] w-full min-w-[400px] max-w-[450px] overflow-x-auto">
+		<VirtualScroll items={$monthlyState}>
+			<div
+				class="{MONTHLY_LAYOUT} sticky top-0 auto-rows-auto overflow-hidden border border-x-0 bg-gradient-to-r from-zinc-800 to-stone-800"
+				slot="header"
+			>
+				<div class="py-2 text-left">Month</div>
+				<div class="py-2 text-right">Incomes</div>
+				<div class="py-2 text-right">Expenses</div>
+				<div class="py-2 text-right">Spared</div>
+			</div>
+			<MonthlyItem slot="row" let:item monthly={item} />
+		</VirtualScroll>
+	</div>
+{/if}
---
+++ src/routes/(app)/reports/constants.ts
@@ -0,0 +1,1 @@
+export const MONTHLY_LAYOUT = 'grid gap-1 grid-cols-[100px_0.3fr_0.3fr_0.3fr]';
