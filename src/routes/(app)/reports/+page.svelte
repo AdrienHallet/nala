fc7e70e1043cc82a4441480024fe882c8a8ac8ef
Adrien Hallet <adrienhallet01@gmail.com>
2023-03-22 22:17:52 +0100

    Feat: monthly expense/income graph

--- src/routes/(app)/reports/+page.svelte
+++ src/routes/(app)/reports/+page.svelte
@@ -3,6 +3,8 @@
 	import { loading } from '$core/loading/state.js';
 	import VirtualScroll from '$ui/components/virtual-scroll.svelte';
 	import Loading from '$ui/components/loading.svelte';
+	import Monthly from '$ui/graphs/monthly/Monthly.svelte';
+	import { dailyState } from '../../../core/database/balance/state';
 	import { getCategories } from '../../../core/database/category/operations';
 	import { getTransactions } from '../../../core/database/transaction/operations';
 	import { MONTHLY_LAYOUT } from './constants.js';
@@ -10,23 +12,30 @@
 
 	getTransactions();
 	getCategories();
+
+	$dailyState;
 </script>
 
 {#if $loading.database || $loading.transactions || $loading.categories}
 	<Loading />
 {:else}
-	<div class="h-[calc(100vh-66px)] w-full min-w-[400px] max-w-[450px] overflow-x-auto">
-		<VirtualScroll items={$monthlyState}>
-			<div
-				class="{MONTHLY_LAYOUT} sticky top-0 auto-rows-auto overflow-hidden border border-x-0 bg-gradient-to-r from-zinc-800 to-stone-800"
-				slot="header"
-			>
-				<div class="py-2 text-left">Month</div>
-				<div class="py-2 text-right">Incomes</div>
-				<div class="py-2 text-right">Expenses</div>
-				<div class="py-2 text-right">Spared</div>
-			</div>
-			<MonthlyItem slot="row" let:item monthly={item} />
-		</VirtualScroll>
+	<div class="flex flex-row">
+		<div class="h-[calc(100vh-4rem)] w-full min-w-[400px] max-w-[450px] overflow-x-auto">
+			<VirtualScroll items={$monthlyState} classes="">
+				<div
+					class="{MONTHLY_LAYOUT} sticky top-0 auto-rows-auto overflow-hidden border border-x-0 bg-gradient-to-r from-zinc-800 to-stone-800"
+					slot="header"
+				>
+					<div class="py-2 text-left">Month</div>
+					<div class="py-2 text-right">Incomes</div>
+					<div class="py-2 text-right">Expenses</div>
+					<div class="py-2 text-right">Spared</div>
+				</div>
+				<MonthlyItem slot="row" let:item monthly={item} />
+			</VirtualScroll>
+		</div>
+		<div class="grow">
+			<Monthly monthlies={$monthlyState} />
+		</div>
 	</div>
 {/if}
