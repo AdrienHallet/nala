e394aaeeca816a406cb511ce31cdc572be3c143d
Adrien Hallet <adrienhallet01@gmail.com>
2023-05-02 21:17:44 +0200

    fix: Simplier item updates

--- src/routes/(app)/categories/category-item.svelte
+++ src/routes/(app)/categories/category-item.svelte
@@ -4,26 +4,18 @@
 	import { CATEGORIES_LAYOUT } from './constants.js';
 
 	export let category: Category;
-	let originalCategory: Category | undefined = undefined;
-
-	$: category, onCategoryChange();
 
 	function onCategoryChange() {
-		if (!originalCategory || originalCategory.id != category.id) {
-			// Render of the component with a new category (might be first or n time)
-			originalCategory = category;
-			return;
-		}
-		category = Object.assign(new Category(), category);
-		if (!category.equals(originalCategory)) {
-			// Category was updated and modified (sometimes update fired because of this bug: https://github.com/sveltejs/svelte/issues/5689)
-			updateCategory(category);
-			originalCategory = category;
-		}
+		updateCategory(category);
 	}
 </script>
 
 <div class={CATEGORIES_LAYOUT}>
-	<input type="text" class="ring-inset" bind:value={category.name} />
-	<input type="text" class="ring-inset" bind:value={category.description} />
+	<input type="text" class="ring-inset" bind:value={category.name} on:input={onCategoryChange} />
+	<input
+		type="text"
+		class="ring-inset"
+		bind:value={category.description}
+		on:input={onCategoryChange}
+	/>
 </div>
--- src/routes/(app)/transactions/transaction-item.svelte
+++ src/routes/(app)/transactions/transaction-item.svelte
@@ -8,12 +8,9 @@
 	import { TRANSACTIONS_LAYOUT } from './constants.js';
 
 	export let transaction: Transaction;
-	let originalTransaction: Transaction | undefined = undefined;
 	let amountDisplay = ((transaction.amount || 0) / 100).toFixed(2);
 	let component: Element;
 	let focused = false;
-
-	$: transaction, onTransactionChange();
 
 	onMount(() => {
 		component.addEventListener('focusin', () => (focused = true));
@@ -21,27 +18,22 @@
 	});
 
 	function onTransactionChange() {
-		if (!originalTransaction || originalTransaction.id != transaction.id) {
-			// Render of the component with a new transaction (might be first or n time)
-			originalTransaction = transaction;
-			amountDisplay = ((transaction.amount || 0) / 100).toFixed(2);
-			return;
-		}
-		transaction = Object.assign(new Transaction(), transaction);
-		if (!transaction.equals(originalTransaction)) {
-			// Transaction was updated and modified (sometimes update fired because of this bug: https://github.com/sveltejs/svelte/issues/5689)
-			updateTransaction(transaction);
-			originalTransaction = transaction;
-		}
+		updateTransaction(transaction);
 	}
 
 	function onAmountInput() {
 		transaction.amount = Math.round(parseFloat(amountDisplay || '0') * 100);
+		onTransactionChange();
 	}
 </script>
 
 <div bind:this={component} class={TRANSACTIONS_LAYOUT}>
-	<input type="date" class="ring-inset" bind:value={transaction.date} />
+	<input
+		type="date"
+		class="ring-inset"
+		bind:value={transaction.date}
+		on:input={onTransactionChange}
+	/>
 	<input class="hidden" bind:value={transaction.amount} step="any" type="number" />
 	<input
 		class="w-full bg-transparent text-right ring-inset"
@@ -52,13 +44,18 @@
 			(event.currentTarget.value = parseFloat(event.currentTarget.value || 0).toFixed(2))}
 		on:input={onAmountInput}
 	/>
-	<select class="ring-inset" bind:value={transaction.categoryId}>
+	<select class="ring-inset" bind:value={transaction.categoryId} on:input={onTransactionChange}>
 		<option value="" />
 		{#each $categories as category}
 			<option value={category.id}>{category.name}</option>
 		{/each}
 	</select>
-	<input type="text" class="ring-inset" bind:value={transaction.title} />
+	<input
+		type="text"
+		class="ring-inset"
+		bind:value={transaction.title}
+		on:input={onTransactionChange}
+	/>
 	<div
 		on:keyup={() => deleteTransaction(transaction)}
 		on:click={() => deleteTransaction(transaction)}
